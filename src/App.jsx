9ce25c0b0e6d1f9b722509cf48a7b75caeabe649
Mauri Schefer <95150798+MauriMS@users.noreply.github.com>
2025-11-27 17:48:06 -0300

    Cambios en los componentes del app

--- src/App.jsx
+++ src/App.jsx
@@ -5,33 +5,19 @@ import Legend from "./components/Legend";
 import Tools from "./components/Tools";
 import Editor from "./components/Editor";
 import "./App.css";
+import Nav from "./nav";
 
 function App() {
-  const [sidebarOpen, setSidebarOpen] = useState(true);
+  
 
   return (
     <MapWrapper>
-      {/* Botón de Toggle para el Sidebar */}
-      <button
-        className={`sidebar-toggle ${sidebarOpen ? "open" : ""}`}
-        onClick={() => setSidebarOpen(!sidebarOpen)}
-        title={sidebarOpen ? "Ocultar menú" : "Mostrar menú"}
-      >
-        {sidebarOpen ? "◀" : "▶"}
-      </button>
 
-      {/* Barra Lateral con clase condicional */}
-      <div className={`sidebar ${sidebarOpen ? "visible" : "hidden"}`}>
-        <h2 className="title">SIG TPI 2025</h2>
 
-        {/* Contenedor con scroll para los paneles */}
-        <div className="sidebar-content">
-          <LayerControl />
-          <Tools />
-          <Legend />
-          <Editor />
-        </div>
-      </div>
+
+
+      <Nav/>
+
     </MapWrapper>
   );
 }
